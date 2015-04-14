44ad4bb4c54339aef4156e7421150f22c3b36137
Luke Shiner <luke@lukeshiner.com>
2015-04-14 16:33:54 +0100

    add validation to all fields on pages basic and non-var ex prop

--- public/scripts/forms.js
+++ public/scripts/forms.js
@@ -37,6 +37,9 @@ function setFormStyle(){
 
 illegalChars = ['"', "'", '~', ';', '<', '>', '\\', '/'];
 
+priceRegEx = /^([0-9]*((.)[0-9]{0,2}))$/;
+barcodeRegEx = /^\d{12,13}$/;
+
 setFormStyle();
 
 $('#content').find($('form')).find($('input, textarea, checkbox, select')).each(function (){
@@ -55,23 +58,95 @@ function validate(input) {
         itemTitleValidate(input);
     }
     
-    var priceFields = [/^retail_price\d*$/i, /^purchase_price\d*$/i, /^ebay_price\d*$/i, /^am_price\d*$/i];    
+    var priceFields = [/^retail_price\d*$/i, /^purchase_price\d*$/i, /^ebay_price\d*$/i, /^am_price\d*$/i];
     for (key in priceFields) {
         if (priceFields[key].test(inputId)) {
             priceValidate(input);
         }
     }
     
+    var dimensions = [/^height\d*$/i, /^width\d*$/i, /^depth\d*$/i];
+    for (key in dimensions) {
+        if (dimensions[key].test(inputId)) {
+            dimensionValidate(input);
+        }
+    }
+    
     if (inputId === 'short_description') {
         shortDescriptionValidate(input);
     }
+    
+    var textFields = [/^manufacturer\d*$/i, /^brand\d*$/i, /^size\d*$/i, /^colour\d*$/i, /^style\d*$/i, /^material\d*$/i];
+    for (key in textFields) {
+        if (textFields[key].test(inputId)) {
+            input.blur(function() {
+                removeIllegalChars(input);
+            });
+        }
+    }
+    
+    if (/^weight\d*$/i.test(inputId)) {
+        weightValidate(input);
+    }
+    
+    if (inputId == 'barcode') {
+        barcodeValidate(input);
+    }
+}
+
+function weightValidate(input) {
+    input.blur(function() {
+        var reg = /^[0-9]*$/;
+        if (!(input.val() === '')) {
+            if (!(reg.test(input.val()))) {
+                writeError(input, "Must be a numeric value");
+            }
+        }
+    });
+    
+    clearError(input);
+}
+
+function dimensionValidate(input) {
+    input.blur(function() {
+        var value = input.val();
+        if (!(priceRegEx.test(value))) {
+            writeError(input, 'Must be a valid number or decimal');
+        }
+    });
+    
+    clearError(input);
+}
+
+function barcodeValidate(input) {
+    input.blur(function() {
+        if (!(input.val() === '')) {
+            if (!(barcodeRegEx.test(input.val()))) {
+                writeError(input, 'Must be valid barcode');
+            }
+        }
+    });
+    
+    clearError(input);
+}
+
+function removeIllegalChars(input) {
+    var value = input.val();
+    for (key in illegalChars) {
+        value = value.replace(illegalChars[key], '');
+    }
+    input.val(value);
 }
 
 function priceValidate(input) {
-    var value = input.val();
     input.blur(function() {
+        var value = input.val();
+        console.log(value);
         if (value === '') {
-            writeError(input, 'Please add price');
+            writeError(input, 'Field is required');
+        }
+        if (!(priceRegEx.test(value))) {
+            writeError(input, 'Must be a valid number or decimal');
         }
     });
     
@@ -81,10 +156,7 @@ function priceValidate(input) {
 function itemTitleValidate(input) {
     input.blur(function() {
         var value = input.val();
-        for (key in illegalChars) {
-            value = value.replace(illegalChars[key], '');
-        }
-        input.val(value);
+        removeIllegalChars(input);
         
         if (input.val().length == 0) { // if empty
             writeError(input, 'Product title is required');
@@ -100,13 +172,9 @@ function itemTitleValidate(input) {
 }
 
 function shortDescriptionValidate(input) {
-    
     input.blur(function() {
         var value = input.val();
-        for (key in illegalChars) {
-            value = value.replace(illegalChars[key], '');
-        }
-        input.val(value);
+        removeIllegalChars(input);
         
         if (input.val().length == 0) { // if empty
             writeError(input, 'Product description is required');
@@ -127,10 +195,9 @@ function writeError(input, text) {
         }
     });
     
-    if (written === true) {
-        return '';
+    if (!(written === true)) {
+        input.after('<p class=error>' + text + '</p>');
     }
-    input.after('<p class=error>' + text + '</p>');
 }
 
 function clearError(input) {
