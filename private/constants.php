cf51b93650d069436dd821537f107e5271dcbf70
Luke Shiner <luke@lukeshiner.com>
2015-04-16 14:59:48 +0100

    renames database stcadmin > stcadministration to align with live version

--- private/constants.php
+++ private/constants.php
@@ -2,7 +2,7 @@
 $DB_HOST = "localhost";
 $DB_USER = "axevalley";
 $DB_PASS = "fred";
-$DB_DB = "stcadmin";
+$DB_DB = "stcadministration";
 $CSVFILEPATH = dirname($_SERVER['DOCUMENT_ROOT']) . '/private/new_product_files/current/';
 $OLDCSVFILEPATH = dirname($_SERVER['DOCUMENT_ROOT']) . '/private/new_product_files/archive/';
 $IMAGEURLPATH = "http://stcadmin.localhost/new_product/image.php?id=";
