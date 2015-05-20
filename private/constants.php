500b9570d33b5a0b64c9f006ada67ff0dc8855ac
Luke Shiner <luke@lukeshiner.com>
2015-05-20 12:16:27 +0100

    Changed server credentials to use stcadmin.stcstores.co.uk database

--- private/constants.php
+++ private/constants.php
@@ -1,8 +1,8 @@
 <?php
-$DB_HOST = "localhost";
-$DB_USER = "axevalley";
-$DB_PASS = "fred";
-$DB_DB = "stcadministration";
+$DB_HOST = "mysql.stcadmin.stcstores.co.uk";
+$DB_USER = "seatontrading";
+$DB_PASS = "Cosworth1";
+$DB_DB = "seatontrading";
 $CSVFILEPATH = dirname($_SERVER['DOCUMENT_ROOT']) . '/private/new_product_files/current/';
 $OLDCSVFILEPATH = dirname($_SERVER['DOCUMENT_ROOT']) . '/private/new_product_files/archive/';
 $IMAGEURLPATH = "http://stcadmin.localhost/new_product/image.php?id=";
