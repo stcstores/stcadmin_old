bca0310f5c06afdc2b3e0903073534439a7e912c
Luke Shiner <luke@lukeshiner.com>
2015-05-20 16:21:26 +0100

    Adds keyfields.php (was not added in last commit)

---
+++ public/new_product/keyfields.php
@@ -0,0 +1,18 @@
+<?php
+require_once(dirname($_SERVER['DOCUMENT_ROOT']) . '/private/config.php');
+require_once($CONFIG['include']);
+checkLogin();
+require_once($CONFIG['header']);
+?>
+
+<div class=pagebox>
+    <h2>Key Fields</h2>
+    <p>Key fields are the ways in which a variation product varies.</p>
+    <p>
+        For instance if a shirt comes in two colours and five sizes then 'Size' and 'Colour' would be key fields. As they all share a style 'Style' would not be a key field.
+        The values in the key fields become part of the variation title. Selecting the correct key fields ensures no two variations can share a title.
+    </p>
+</div>
+
+<?php
+require_once($CONFIG['footer']);
