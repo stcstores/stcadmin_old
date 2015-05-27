6611cceba70c00d85d06ca84beaa49746641d4bd
Luke Shiner <luke@lukeshiner.com>
2015-05-27 10:40:30 +0100

    Changes image page url to use stcadmin.stcstores.co.uk

--- private/constants.php
+++ private/constants.php
@@ -5,5 +5,5 @@ $DB_PASS = "Cosworth1";
 $DB_DB = "seatontrading";
 $CSVFILEPATH = dirname($_SERVER['DOCUMENT_ROOT']) . '/private/new_product_files/current/';
 $OLDCSVFILEPATH = dirname($_SERVER['DOCUMENT_ROOT']) . '/private/new_product_files/archive/';
-$IMAGEURLPATH = "http://stcadmin.localhost/new_product/image.php?id=";
+$IMAGEURLPATH = "http://stcadmin.stcstores.co.uk/new_product/image.php?id=";
 ?>
