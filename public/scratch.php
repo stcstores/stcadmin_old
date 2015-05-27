ebbdc570ad402f7e77318914f822806eac4931d9
Luke Shiner <luke@lukeshiner.com>
2015-05-27 10:54:35 +0100

    Empties scratch.php

--- public/scratch.php
+++ public/scratch.php
@@ -3,7 +3,7 @@ require_once('../private/include.php');
 include('../private/header.php');
 include('css/colour_scheme.php');
 
-$_SESSION['new_product']->details['int_shipping']->set(false);
+
 
 
 
