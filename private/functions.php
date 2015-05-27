058250bd1b6ed19fb4d8ee7076896eb15ccc07d5
Luke Shiner <luke@lukeshiner.com>
2015-05-27 15:38:09 +0100

    Adds list_pending_products function to functions.php to get a list of products currently in the pending csvs

--- private/functions.php
+++ private/functions.php
@@ -182,4 +182,38 @@ function getSpecialCharacters() {
     return $results;
 }
 
+function list_pending_products() {
+    $basicInfoCsv = new BasicInfoFile();
+    $basicInfo = $basicInfoCsv->read();
+    $newVarCsv = new NewVarGroupFile();
+    $newVar = $newVarCsv->read();
+    $addVarCsv = new AddToVarGroupFile();
+    $addVar = $addVarCsv->read();
+    $pending_products = array();
+    
+    $i = 0;
+    foreach ($newVar['VariationSKU'] as $row) {
+        $pending_product = array();
+        $pending_product['SKU'] = $newVar['VariationSKU'][$i];
+        $pending_product['Title'] = $newVar['VariationGroupName'][$i];
+        $pending_products[] = $pending_product;
+        $i++;
+    }
+    
+    $varSKUs = $addVar['SKU'];
+    
+    $i = 0;
+    foreach($basicInfo['SKU'] as $SKU) {
+        if (!(in_array($SKU, $varSKUs))) {
+            $pending_product = array();
+            $pending_product['SKU'] = $basicInfo['SKU'][$i];
+            $pending_product['Title'] = $basicInfo['Title'][$i];
+            $pending_products[] = $pending_product;
+        }
+        $i++;
+    }
+    
+    return $pending_products;       
+}
+
 ?>
