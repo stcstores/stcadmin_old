b8f2a36f98946637b5f1510a24e793a92e6a307b
Luke Shiner <luke@lukeshiner.com>
2015-07-28 17:14:04 +0100

    Adds createGUID function to functions.php

--- private/functions.php
+++ private/functions.php
@@ -226,4 +226,8 @@ function list_pending_products() {
     return $pending_products;       
 }
 
+function createGUID() {
+    return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
+}
+
 ?>
