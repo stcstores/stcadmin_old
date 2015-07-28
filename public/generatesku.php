ebf09250f1dc53e39e1072ce142fce540b35d1df
Luke Shiner <luke@lukeshiner.com>
2015-07-28 16:31:11 +0100

    Checks user is logged in on generatesku.php

--- public/generatesku.php
+++ public/generatesku.php
@@ -1,6 +1,7 @@
 <?php
 require_once(dirname($_SERVER['DOCUMENT_ROOT']) . '/private/config.php');
 require_once($CONFIG['include']);
+checkLogin();
 
 $api = new LinnworksAPI($_SESSION['username'], $_SESSION['password']);
 
