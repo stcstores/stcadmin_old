52a5faf57db5d41215b334b2f8dc30f07ad533cc
Luke Shiner <luke@lukeshiner.com>
2015-10-12 15:42:36 +0100

    Removes test pages from main navigation

--- private/navbar.php
+++ private/navbar.php
@@ -4,6 +4,6 @@
         <li><a href=/new_product/testnewproduct.php >Test Product</a></li>
         <li><a href=/getsku.php >Get SKU</a></li>
         <li><a href=/admin/admin.php >Admin</a></li>
-        <li><a href=/scratch.php >Scratch</a></li>					
-        <li><a href=/cols.php >Colours</a></li>
+        <!--<li><a href=/scratch.php >Scratch</a></li>					-->
+        <!--<li><a href=/cols.php >Colours</a></li>-->
 </ul>
