1c348b787ba2a6a820cdd0a27eefe1a7c888a5a8
Luke Shiner <luke@lukeshiner.com>
2015-10-20 16:52:08 +0100

    Fixes typo in the navigation bar

--- private/navbar.php
+++ private/navbar.php
@@ -3,7 +3,7 @@
         <li><a href=/new_product/new_product_start.php >New Product</a></li>
         <!--<li><a href=/new_product/testnewproduct.php >Test Product</a></li>-->
         <li><a href=/getsku.php >Get SKU</a></li>
-        <li><a href=/get_international_shipping.php </a>Shipping</li>
+        <li><a href=/get_international_shipping.php >Shipping</a></li>
         <li><a href=/admin/admin.php >Admin</a></li>
         <!--<li><a href=/scratch.php >Scratch</a></li>					-->
         <!--<li><a href=/cols.php >Colours</a></li>-->
