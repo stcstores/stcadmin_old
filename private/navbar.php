eab0d029e8e5bffc011561d222689358db5f309b
Luke Shiner <luke@lukeshiner.com>
2015-10-27 13:22:48 +0000

    Updates navbar page to include link to news.php

--- private/navbar.php
+++ private/navbar.php
@@ -5,6 +5,7 @@
         <li><a href=/getsku.php >Get SKU</a></li>
         <li><a href=/get_international_shipping.php >Shipping</a></li>
         <li><a href=/admin/admin.php >Admin</a></li>
+        <li><a href='/admin/news.php/'>News</a></li>
         <!--<li><a href=/scratch.php >Scratch</a></li>					-->
         <!--<li><a href=/cols.php >Colours</a></li>-->
 </ul>
