57f054d4d310c14600876ad45259bdac3a49d3e7
Luke Shiner <luke@lukeshiner.com>
2015-11-04 15:31:39 +0000

    Prevends backspace button sending the browser back a page

--- public/scripts/formstyle.js
+++ public/scripts/formstyle.js
@@ -3,23 +3,23 @@ function setFormStyle(){
     $('input').focus(function() {
         $(this).css('border-color', '#729292');
     });
-    
+
     $('input').blur(function() {
         $(this).removeAttr('style');
     });
-    
+
     $('select').focus(function() {
         $(this).css('border-color', '#729292');
     });
-    
+
     $('select').blur(function() {
         $(this).removeAttr('style');
     });
-    
+
     $('textarea').focus(function() {
         $(this).css('border-color', '#729292');
     });
-    
+
     $('textarea').blur(function() {
         $(this).removeAttr('style');
     });
@@ -40,4 +40,32 @@ $("form").keypress(function(e){
             return false;
         }
    }
+});
+
+// Prevent the backspace key from navigating back.
+$(document).unbind('keydown').bind('keydown', function (event) {
+    var doPrevent = false;
+    if (event.keyCode === 8) {
+        var d = event.srcElement || event.target;
+        if ((d.tagName.toUpperCase() === 'INPUT' &&
+             (
+                 d.type.toUpperCase() === 'TEXT' ||
+                 d.type.toUpperCase() === 'PASSWORD' ||
+                 d.type.toUpperCase() === 'FILE' ||
+                 d.type.toUpperCase() === 'SEARCH' ||
+                 d.type.toUpperCase() === 'EMAIL' ||
+                 d.type.toUpperCase() === 'NUMBER' ||
+                 d.type.toUpperCase() === 'DATE' )
+             ) ||
+             d.tagName.toUpperCase() === 'TEXTAREA') {
+            doPrevent = d.readOnly || d.disabled;
+        }
+        else {
+            doPrevent = true;
+        }
+    }
+
+    if (doPrevent) {
+        event.preventDefault();
+    }
 });
