1bb8347652e2482f2dc1d5a96248bad5948a61d1
Luke Shiner <luke@lukeshiner.com>
2015-11-09 15:53:36 +0000

    Changes get_international_shipping.php to meet standards

--- public/get_international_shipping.php
+++ public/get_international_shipping.php
@@ -6,6 +6,8 @@ require_once($CONFIG['header']);
 
 if (isset($_GET['weight'])) {
     $weight = $_GET['weight'];
+} else {
+    $weight = '';
 }
 
 ?>
@@ -13,41 +15,41 @@ if (isset($_GET['weight'])) {
 <div class=pagebox >
     <form>
         <label for=weight >Weight (g)</label>
-        <input name=weight <?php if (isset($weight)) {echo "value='" . $weight . "' "; } ?>/>
+        <input name=weight <?php echo $weight; ?>/>
         <input type=submit value="Get Shipping Prices" />
     </form>
     <br />
     <div id=result>
         <?php
-            if (isset($weight)) {
-                $intPostagePrices = get_international_shipping($weight);
-                ?>
-                <table class=form_section id=shipping_table >
-                    <tr>
-                        <th>France</th>
-                        <th>Germany</th>
-                        <th>Europe</th>
-                        <th>USA</th>
-                        <th>Austraila</th>
-                        <th>Rest of World</th>
-                    </tr>
-                    <tr>
-                        <td>&pound;<?php echo sprintf("%0.2f",$intPostagePrices['fr']); ?></td>
-                        <td>&pound;<?php echo sprintf("%0.2f",$intPostagePrices['de']); ?></td>
-                        <td>&pound;<?php echo sprintf("%0.2f",$intPostagePrices['eu']); ?></td>
-                        <td>&pound;<?php echo sprintf("%0.2f",$intPostagePrices['usa']); ?></td>
-                        <td>&pound;<?php echo sprintf("%0.2f",$intPostagePrices['aus']); ?></td>
-                        <td>&pound;<?php echo sprintf("%0.2f",$intPostagePrices['row']); ?></td>
-                    </tr>
-                </table>
+        if (isset($weight)) {
+            $intPostagePrices = get_international_shipping($weight);
+            ?>
+            <table class=form_section id=shipping_table >
+                <tr>
+                    <th>France</th>
+                    <th>Germany</th>
+                    <th>Europe</th>
+                    <th>USA</th>
+                    <th>Austraila</th>
+                    <th>Rest of World</th>
+                </tr>
+                <tr>
+                    <td>&pound;<?php echo sprintf("%0.2f", $intPostagePrices['fr']); ?></td>
+                    <td>&pound;<?php echo sprintf("%0.2f", $intPostagePrices['de']); ?></td>
+                    <td>&pound;<?php echo sprintf("%0.2f", $intPostagePrices['eu']); ?></td>
+                    <td>&pound;<?php echo sprintf("%0.2f", $intPostagePrices['usa']); ?></td>
+                    <td>&pound;<?php echo sprintf("%0.2f", $intPostagePrices['aus']); ?></td>
+                    <td>&pound;<?php echo sprintf("%0.2f", $intPostagePrices['row']); ?></td>
+                </tr>
+            </table>
         <?php
-            }
+        }
         ?>
     </div>
 </div>
 
 <script>
-    
+
 </script>
 
 <?php
