c149565e59404d6225150d49644ff0e9b7c113e5
Luke Shiner <luke@lukeshiner.com>
2015-11-10 10:39:23 +0000

    Adds var_table page

---
+++ public/new_product/new_linnworks_product_var_table.php
@@ -0,0 +1,86 @@
+<?php
+
+    require_once(dirname($_SERVER['DOCUMENT_ROOT']) . '/private/config.php');
+    require_once($CONFIG['include']);
+    checkLogin();
+
+if (isset($_SESSION['new_product'])) {
+    $product = $_SESSION['new_product'];
+} else {
+    header('Location: new_product_start.php');
+    exit();
+}
+
+if ( !empty($_POST) ) {
+
+    add_variation($product);
+
+    if (isset($_POST['previous'])) {
+        header('Location: new_linnworks_product_1_basic_info.php');
+        exit();
+    }
+
+
+    if ( true ) { // error check
+        $_SESSION['new_product'] = $product;
+        header('Location: imageupload.php');
+        exit();
+    }
+}
+
+require_once($CONFIG['header']);
+
+$fields = getVarSetupFields();
+$values = getVarSetupValues();
+?>
+
+<script>
+    productName = '<?php echo $product->details['item_title']->text; ?>';
+    var fields = <?php echo json_encode($fields); ?>;
+    var values = <?php echo json_encode($values); ?>;
+    keyFields = <?php echo json_encode($product->keyFields); ?>;
+</script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js"></script>
+<script src="/scripts/jquery.doubleScroll.js"></script>
+
+<div class="">
+    <h2>Set Variations for <?php echo $product->details['item_title']->text; ?></h2>
+    <div id="var_error" class="hidden" ></div>
+    <form method="post" id="var_form" enctype="multipart/form-data">
+        <table id="var_setup" class="form_section" >
+
+        </table>
+        <table class="form_nav">
+            <tr>
+                <td>
+                    <input value="<< Previous" type="submit" name="previous" />
+                    <input value="Next >>" type="submit" name="next" />
+                </td>
+            </tr>
+        </table>
+    </form>
+</div>
+
+<script src="/scripts/var_form_validate.js"></script>
+<script src="/scripts/variation_table.js"></script>
+
+<?php
+    $_SESSION['new_product'] = $product;
+?>
+<script>
+    keyFields = <?php echo json_encode(getKeyFields()); ?>;
+</script>
+<script>
+    shippingPrice = <?php echo $product->details['shipping_price']->value; ?>;
+</script>
+<script src=/scripts/formstyle.js ></script>
+<script src=/scripts/validation.js ></script>
+<script>product_title = '<?php echo $product->details['item_title']->text;?>'</script>
+<script>product_price = '<?php echo $product->details['retail_price']->text;?>'</script>
+<script>product_description = <?php echo json_encode($product->details['short_description']->text);?></script>
+
+<?php
+
+include($CONFIG['footer']);
+
+?>
