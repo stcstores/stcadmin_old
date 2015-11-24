b5ac3ccbd2736af7a1f6ff930f8982c6bfbdce63
Luke Shiner <luke@lukeshiner.com>
2015-11-24 11:40:34 +0000

    Removes depricated entries in config.php and constants.php

--- private/constants.php
+++ private/constants.php
@@ -1,9 +1,4 @@
 <?php
-$DB_HOST = "mysql.stcadmin.stcstores.co.uk";
-$DB_USER = "seatontrading";
-$DB_PASS = "Cosworth1";
-$DB_DB = "seatontrading";
 $CSVFILEPATH = dirname($_SERVER['DOCUMENT_ROOT']) . '/private/new_product_files/current/';
 $OLDCSVFILEPATH = dirname($_SERVER['DOCUMENT_ROOT']) . '/private/new_product_files/archive/';
-$IMAGEURLPATH = "http://stcadmin.stcstores.co.uk/new_product/image.php?id=";
 ?>
