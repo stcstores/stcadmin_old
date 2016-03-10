96c41a9c417101d60c75b85aaa54113e2c407d0e
Luke Shiner <luke@lukeshiner.com>
2016-03-10 13:13:21 +0000

    Basic working search table in relocation/index.php

--- public/relocation/index.php
+++ public/relocation/index.php
@@ -7,9 +7,54 @@ require_once($CONFIG['header']);
 $api = new LinnworksAPI\LinnworksAPI($_SESSION['username'], $_SESSION['password']);
 $database = new STCAdmin\Database();
 
-$itemList = $api->search_inventory_item_title('FW799');
+?>
+<form method="GET">
+    <table>
+        <tr>
+            <td><label for="search_string">Search Title</label></td>
+            <td><input type="text" name="search_string"></td>
+        </tr>
+        <tr>
+            <td><label for="sku">Search By SKU</label></td>
+            <td><input type="text" name="sku"></td>
+        </tr>
+        <tr>
+            <td><input type="submit"></td>
+        </tr>
+    </table>
+</form>
+<br />
+<?php
 
-#print_r($itemList);
+if (isset($_GET['search_string'])) {
+    $itemList = $api->search_inventory_item_title($_GET['search_string']);
+} elseif (isset($_GET['sku'])) {
+    $itemList = $api->get_variation_group_inventory_item_by_SKU($_GET['sku']);
+}
+
+if (isset($itemList)) {
+    writeTable($itemList);
+}
+function writeTable($itemList) {
+    ?>
+    <table class="item_details">
+        <?php
+        foreach ($itemList as $item) {
+            ?>
+            <tr>
+                <td><?php echo $item['sku']; ?></td>
+                <td><?php echo $item['title']; ?></td>
+                <td>
+                    <input type="text">
+                </td>
+            </tr>
+            <?php
+        }
+        ?>
+    </table>
+    <?php
+}
+
 
 
 include($CONFIG['footer']);
