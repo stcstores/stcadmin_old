91d88d031d60d38df029250e3b89094e8f29e74c
Luke Shiner <luke@lukeshiner.com>
2016-06-14 14:04:35 +0100

    Fixes photos link on homepage

--- public/index.php
+++ public/index.php
@@ -16,7 +16,7 @@ $result = $database -> selectQuery($query);
     <p>From here you Can:</p>
     <ul>
         <li><a href='new_product/new_product_start.php' >Add a new product to Linnworks</a></li>
-        <li><a href='new_product/photos'>Download Product Images</a></li>
+        <li><a href='/photos'>Download Product Images</a></li>
         <li><a href="/dispatch">Bulk process open orders</a></li>
         <li><a href=getsku.php >Get an unused product SKU</a></li>
         <li><a href='get_international_shipping.php' >Find international shipping rates for products</a></li>
